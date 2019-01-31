6083a3a1be07caf775c51d2b19690c3a30a118a7
Richard Joffray <rjoffray@Richards-MacBook-Pro.local>
2019-01-31 15:20:38 -0600

    misc fixes

--- src/admin/public/directives/chooseFile.directive.js
+++ src/admin/public/directives/chooseFile.directive.js
@@ -1,7 +1,7 @@
 (function(app){
     'use strict';
     
-    app.directive('chooseFile', function () {
+    app.directive('chooseFile', function ($log) {
     return {
         
         link: function (scope, elem, attrs) {
@@ -10,7 +10,7 @@
         // let gallery = attrs.galleryObj
         var index = attrs.index
         var input = angular.element(elem[0].querySelector('input'))
-            console.log(input)
+            //$log.info("Input Element", input)
         button.bind('click', function () {
             input[0].click()
         })
@@ -19,7 +19,7 @@
             var files = e.target.files
             if (files[0]) {
                 //$scope.fileName = files[0].name
-                console.log(files[0].name)
+                //$log.info(files[0].name)
                 scope.addImageToSlide(scope.selectedGallery, index, files[0].name, files)
             } else {
                 scope.fileName = null
